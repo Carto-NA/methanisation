c01b477de89d0f30a39c96fe664665ca8b97d39b
Carto-NA <63336882+Carto-NA@users.noreply.github.com>
2020-04-08 09:47:47 +0200

    Create methanisation_00_trace.sql

---
+++ sql/methanisation_00_trace.sql
@@ -0,0 +1,13 @@
+/*METHANISATION V1.0*/
+/*Creation du fichier "00_trace" qui permet de suivre l'évolution du code*/
+/* methanisation_00_trace.sql */
+/*PostGIS*/
+
+/* Propriétaire : Conseil régional Nouvelle-Aquitaine - http://cartographie.nouvelle-aquitaine.fr/ */
+/* Auteur : Tony VINCENT*/
+
+
+/*
+#################################################################### SUIVI CODE SQL ####################################################################
+2020-03-31 : TV / initialisation du code uniquement
+2020-04-08 : TV / initialisation de la structure de la base de données sur la méthanisation
